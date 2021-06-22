9e5e2524841a057b0dfe5d30a51f0f61b27f6b72
MEGY MANON <manon.megy@gmail.com>
2021-06-22 10:41:57 +0200

    build

---
+++ Source/RockGen/RockGen.h
@@ -0,0 +1,4 @@
+void makeRock( int ***tab,int W,int H);
+void bakeHole( int ***tab, int W, int H);
+vector crunchSpeed(vector vect);
+void RockPanel (int nC, int **CellData, int DataSizeX, int DataSizeY);
